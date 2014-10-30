8fe4938a56d4f260e950d314cb32960b9f49ac84
Mark T. Holder <mtholder@gmail.com>
2014-10-30 15:18:12 +0100

    a group of definitions that lead to -inf lnL

--- src/test/TestingDefs.h
+++ src/test/TestingDefs.h
@@ -1,9 +1,9 @@
 #ifndef TESTING_DEFS_H
 #define TESTING_DEFS_H
-#define READ_MORPHO_AS_DNA
+//#define READ_MORPHO_AS_DNA
 #define USE_GTR_RATE_MAT
 //#define USE_3_STATES
-#define USE_TIME_TREE
-#define USE_NUCLEOTIDE
-#define USE_RATE_HET
+//#define USE_TIME_TREE
+//#define USE_NUCLEOTIDE
+//#define USE_RATE_HET
 #endif
